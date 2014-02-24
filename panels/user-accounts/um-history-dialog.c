41429c3a61d6efbdc119e2de273ad5c69a92102c
Sebastien Bacher <seb128@ubuntu.com>
2014-02-24 10:46:04 +0100

    the type is "pts" not "pty", should be correct this time

--- panels/user-accounts/um-history-dialog.c
+++ panels/user-accounts/um-history-dialog.c
@@ -240,7 +240,7 @@ show_week (UmHistoryDialog *um)
                 /* Display only x-session and tty records */
                 if (!g_str_has_prefix (history.type, ":") &&
                     !g_str_has_prefix (history.type, "tty")&&
-                    !g_str_has_prefix (history.type, "pty")) {
+                    !g_str_has_prefix (history.type, "pts")) {
                         continue;
                 }
 
